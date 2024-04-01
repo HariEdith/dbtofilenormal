def9e0f00a200a6a7413699b6adfd80637287cb5
hariharan.e <selvakumar.r@bluescopetech.com>
2024-04-01 18:48:45 +0530

    updated

--- src/main/java/com/example/demo/DataProcessor.java
+++ src/main/java/com/example/demo/DataProcessor.java
@@ -1,43 +1,40 @@
+
 package com.example.demo;
 
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import org.springframework.stereotype.Component;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.Map;
 
+@Component
 public class DataProcessor {
 
     private static final String FILE_NAME = "file_name";
     private static final String FILE_CONTENT = "file_content";
 
     public Message<File> processData(Message<Map<String, Object>> message) throws IOException {
-      
         Map<String, Object> payload = message.getPayload();
         MessageHeaders headers = message.getHeaders();
 
-        
-        byte[] fileContent = (byte[]) payload.get(FILE_CONTENT);
+        byte[] base64Content = (byte[]) payload.get(FILE_CONTENT);
         String fileName = String.valueOf(payload.get(FILE_NAME));
 
-       
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Content);
+        String decodedContent = new String(decodedBytes, StandardCharsets.UTF_8);
+
         File outputFile = new File("D:/Hari/demo_projects/destination/" + fileName);
 
-        try (OutputStream os = new FileOutputStream(outputFile)) {
-            
-            os.write(fileContent);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
+            writer.write(decodedContent);
             System.out.println("File copied to: " + outputFile.getAbsolutePath());
         } catch (IOException e) {
-           
             e.printStackTrace();
-           
         }
 
-       
         return MessageBuilder.withPayload(outputFile).build();
     }
 }
